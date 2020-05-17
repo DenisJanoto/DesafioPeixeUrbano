f7fbb5314be600ac6709560602eb41cbf9c2cd68
Denis Janoto <denis.janoto@icloud.com>
2020-05-17 10:00:07 -0300

    SecondCommit

---
+++ DesafioPeixeUrbano/Controller/ApiController.swift
@@ -0,0 +1,9 @@
+//
+//  ApiController.swift
+//  DesafioPeixeUrbano
+//
+//  Created by Denis Janoto on 13/05/20.
+//  Copyright © 2020 Denis Janoto. All rights reserved.
+//
+
+import Foundation
---
+++ DesafioPeixeUrbano/Model/receivedJson.swift
@@ -0,0 +1,16 @@
+//
+//  receivedJson.swift
+//  DesafioPeixeUrbano
+//
+//  Created by Denis Janoto on 13/05/20.
+//  Copyright © 2020 Denis Janoto. All rights reserved.
+//
+
+import Foundation
+
+
+class receivedJson{
+    
+    
+    
+}
